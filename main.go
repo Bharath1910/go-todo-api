1b76199f5e83c02e9f29deb28b911b44fbfbe425
Bharath1910 <av.bharath@proton.me>
2023-02-22 17:38:41 +0530

    Completed database connection

--- main.go
+++ main.go
@@ -1,10 +1,38 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func collection(database string, coll string) *mongo.Collection {
+	fmt.Println("Connecting to MongoDB...")
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		fmt.Println(err)
+	}
+	collection := client.Database(database).Collection(coll)
+	fmt.Println("Connected to MongoDB!")
+
+	return collection
+}
+
+func getAllTodos(res http.ResponseWriter, req *http.Request) {
+
+}
+
+func getTodo(res http.ResponseWriter, req *http.Request) {
+
+}
 
 func InitiateRouter() {
 	r := mux.NewRouter()
@@ -15,5 +43,5 @@ func InitiateRouter() {
 }
 
 func main() {
-
+	InitiateRouter()
 }
