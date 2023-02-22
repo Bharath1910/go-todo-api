2c14e6647d604ef0523548ea8dac2f0055887844
Bharath1910 <av.bharath@proton.me>
2023-02-22 17:59:34 +0530

    completed getAllTodos method

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -26,7 +28,44 @@ func collection(database string, coll string) *mongo.Collection {
 	return collection
 }
 
+type TodoData struct {
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+	Uuid      string `json:"uuid"`
+}
+type Users struct {
+	Username string     `json:"username"`
+	Password string     `json:"password"`
+	TodoData []TodoData `json:"todoData"`
+}
+
 func getAllTodos(res http.ResponseWriter, req *http.Request) {
+	allTodo := []Users{}
+
+	collection := collection("Todo", "users")
+	cur, err := collection.Find(context.Background(), bson.D{})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for cur.Next(context.Background()) {
+		var todo Users
+		err := cur.Decode(&todo)
+		if err != nil {
+			fmt.Println(err)
+		}
+		allTodo = append(allTodo, todo)
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(http.StatusOK)
+
+	todoJSON, err := json.Marshal(allTodo)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	res.Write(todoJSON)
 
 }
 
